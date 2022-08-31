a3719a5ebdfd73d105bfc85b9960645603735987
LeonardoGasperin <gasperinlvg@gmail.com>
2022-08-30 22:10:53 -0300

    Learning API's

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const API = "https://eb64-177-73-98-225.ngrok.io"
+
 export default function App() {
+  const [learning, setAPI] = useState([])
+
+  useEffect(() => {
+    fetch(API+"/tasks")
+      .then(async (response) => {
+        const data = await response.json();
+        setAPI(data)
+      })
+      .catch((error) => {
+        alert("ERROR!!! ERROR!!! ERROR!!! ERROR!!! ERROR!!!");
+        console.log(error);
+      })
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {learning.map((ele) => <Text key={ele.id}>{ele.name}</Text>)}
     </View>
   );
 }
